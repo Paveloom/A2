4abffb30980fea2d54c9ce7cf4999e491f7c4afa
Paveloom <paveloom@mail.ru>
2019-05-01 17:25:56 +0300

    Алгоритм Кадане без распараллеливания, за исключением вывода mpiSize.

--- H3/main.f90
+++ H3/main.f90
@@ -2,6 +2,20 @@ program main
 use Task
 implicit none
 
-call MpiCommSizeAndRank
+real(8), allocatable, dimension(:,:) :: A
+integer x1, x2, y1, y2, i, j, thr_num, A_size
+
+     A_size = 100
+     allocate(A(A_size,A_size))
+
+     do i = 1, A_size; do j = 1, A_size
+          
+          A(i,j) = i + j
+     
+     enddo; enddo
+
+     call GetMaxCoordinates(A, x1, y1, x2, y2)
+
+     deallocate(A)
 
 end
