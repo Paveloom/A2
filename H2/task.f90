0b8957833c1f4861d5f02e816ef3a1590722b111
Paveloom <paveloom@mail.ru>
2019-05-01 13:50:41 +0300

    Работающий тестовый алгоритм с выводом.

--- H2/task.f90
+++ H2/task.f90
@@ -1,32 +1,39 @@
 module Task
 use fgsl
 implicit none
+
 contains
 
 function GetSplineValue(x, x_array, y_array)
-    real(fgsl_double), dimension(:), intent(in) :: x_array, y_array
-    real(fgsl_double), intent(in) :: x
-    real(fgsl_double) :: GetSplineValue
-    
-  integer(fgsl_size_t), parameter :: n = 4
-  integer(fgsl_int) :: i, status
-  real(fgsl_double) :: xi, yi
-!      Note: first = last for periodic data
-  type(fgsl_interp_accel) :: acc
-  type(fgsl_spline) :: spline
 
-  spline =  fgsl_spline_alloc(fgsl_interp_cspline_periodic, n)
-  write(6, '(''#m=0,S=5'')')
-  do i=1,n
+real(fgsl_double), dimension(:), intent(in) :: x_array, y_array
+real(fgsl_double), intent(in) :: x
+real(fgsl_double) :: GetSplineValue
+    
+integer(fgsl_size_t) n
+integer(fgsl_int) :: i, status
+
+! Note: first = last for periodic data
+
+    type(fgsl_interp_accel) :: acc
+    type(fgsl_spline) :: spline
+  
+    n = size(x_array)
+
+    spline =  fgsl_spline_alloc(fgsl_interp_cspline_periodic, n)
+  
+    write(6, '(''#m=0,S=5'')')
+    
+    do i = 1, n
      write(6, '(2(F10.5,1X))') x_array(i), y_array(i)
-  end do
+    end do
+    
   write(6, '(''#m=1,S=0'')')
   status = fgsl_spline_init(spline, x_array, y_array)
-  do i=1, 100
-     xi = (1.D0 - dble(i-1)/100.D0) * x_array(1) + dble(i-1)/100.D0 * x_array(n)
-     yi = fgsl_spline_eval(spline, xi, acc)
-     write(6, '(2(F10.5,1X))') xi, yi
-  end do
+  
+     GetSplineValue = fgsl_spline_eval(spline, x, acc)
+     write(6, '(2(F10.5,1X))') x, GetSplineValue
+     
   call fgsl_spline_free (spline)
   call fgsl_interp_accel_free (acc)
     
